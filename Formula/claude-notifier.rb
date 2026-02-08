26ab4ec159fd907abf4de4ef3f7da10b04318261
Mahdi Lazraq <94069699+mlz11@users.noreply.github.com>
2026-02-08 22:22:00 +0100

    claude-notifier 1.11.0

--- Formula/claude-notifier.rb
+++ Formula/claude-notifier.rb
@@ -1,8 +1,8 @@
 class ClaudeNotifier < Formula
   desc "macOS notifications with Claude icon for Claude Code hooks"
   homepage "https://github.com/mlz11/ClaudeNotifier"
-  url "https://github.com/mlz11/ClaudeNotifier/archive/refs/tags/v1.10.0.tar.gz"
-  sha256 "7b37774eed6f277db06de2bd36fce2dfecfa2ab23bae96f42ac8a036731ce7d9"
+  url "https://github.com/mlz11/ClaudeNotifier/archive/refs/tags/v1.11.0.tar.gz"
+  sha256 "b7104e5582ec1c81698a225bc9763beae8fe700d891f12b8c66446f6a276f9d2"
   license "MIT"
 
   depends_on :macos
